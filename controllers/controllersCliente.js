95024bcd83cc49fe8d3685c961182ad269f07d24
julianort11 <ort.na140@gmail.com>
2025-07-31 17:12:06 -0500

    feat: :construction: controllador de clientes en proceso

--- controllers/controllersCliente.js
+++ controllers/controllersCliente.js
@@ -1,3 +1,47 @@
 import chalk, { Chalk } from "chalk";
-import { Cliente } from "../models/cliente.js";
-import { connection } from "../config/db.js";
+import { ServicioCliente } from "../services/servicioCliente.js";
+
+const servicio = new ServicioCliente
+
+export async function controladorAgregarClientes(servicio, datos) {
+    const resultado = await servicio.registrarCliente(datos);
+
+    if (!resultado.ok) {
+        console.log(resultado.mensaje);
+        return;
+    }
+
+    console.log(resultado.mensaje)
+}
+
+export async function controladorListaClientes(servicio, pagina = 1, limite = 10 ) {
+    const resultado = await servicio.obtenerClientesPaginados(pagina, limite);
+
+    console.table(resultado.clientes);
+    console.log(`Pagina ${resultado.paginaActual} de ${resultado.totalPaginas}`);
+}
+
+export async function controladorActualizarClientes(id, nuevosDatos) {
+    try {
+        const resultado = await servicio.actualizarCliente(id,nuevosDatos);
+
+        if(resultado.ok) {
+            console.log(resultado.mensaje);
+        }else {
+            console.warn(resultado.mensaje);
+        }
+    } catch (error) {
+        console.error(chalk.redBright("Error inesperado al actualizar el cliente:"), error.mensaje);
+    }
+}
+
+export async function controladorEliminarCliente(servicio, id) {
+    const resultado = await servicio.eliminarCliente(id);
+
+    if (!resultado.ok) {
+        console.log(resultado.mensaje);
+        return;
+    }
+
+    console.log(resultado. mensaje)
+}
