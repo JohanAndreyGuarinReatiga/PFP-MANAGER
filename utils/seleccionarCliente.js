48e28d7be2ae033362c611dbfc49e598a49f7de6
julianort11 <ort.na140@gmail.com>
2025-08-01 17:57:32 -0500

    feat: :sparkles: mejora de logica y solucion de bugs cliente

--- utils/seleccionarCliente.js
+++ utils/seleccionarCliente.js
@@ -1,17 +1,27 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
 
 export async function seleccionarCliente(clientes) {
+  const opciones = clientes.map((cliente) => ({
+    name: `${cliente.nombre} - ${cliente.empresa}`,
+    value: cliente._id.toString()
+  }));
+
+  opciones.push({
+    name: chalk.yellow("<< Volver"),
+    value: "volver"
+  });
+
   const { clienteId } = await inquirer.prompt([
     {
       type: "list",
       name: "clienteId",
       message: "Selecciona un cliente:",
-      choices: clientes.map((cliente) => ({
-        name: `${cliente.nombre} - ${cliente.empresa}`,
-        value: cliente._id.toString()
-      }))
+      choices: opciones
     }
   ]);
+
+  if (clienteId === "volver") return null;
 
   return clienteId;
 }
